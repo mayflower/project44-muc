70000111f18075fc1607b6a5f602e51a03f41b84
Emanuel Vollmer <emanuel.vollmer@mayflower.de>
2022-09-02 11:26:17 +0200

    Add types

--- backend/createBet/index.ts
+++ backend/createBet/index.ts
@@ -1,8 +1,23 @@
-import {ProxyHandler} from 'aws-lambda'
+import { ProxyHandler } from "aws-lambda";
+
+interface User {
+  userId?: string;
+  nickname: string;
+  mail: string;
+}
+interface Bet {
+  betId?: string;
+  title: string;
+  description: string;
+  criteria: Array<{ criteriaTitle: string; criteriaDescription: string }>;
+  expirationTime: string;
+  creator: User;
+  minimumWager: number;
+  competitors: Array<User>;
+}
 
 const handler: ProxyHandler = async (event, context, callback) => {
-    return {statusCode: 200, body: "{}"}
+  return { statusCode: 200, body: "{}" };
 };
-
 
 module.exports.handler = handler;

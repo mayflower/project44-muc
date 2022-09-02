198a7febab46cb817fe935119d176c83cf684fe9
Ruben Kaiser <ruben2412000@gmail.com>
2022-09-02 13:42:21 +0200

    Route done

--- backend/createBet/index.ts
+++ backend/createBet/index.ts
@@ -1,23 +1,33 @@
-import { ProxyHandler } from "aws-lambda";
+import {APIGatewayProxyHandler} from 'aws-lambda'
+import {Bet} from "./interfaces";
+import {v4 as uuidv4} from 'uuid';
+import {DocumentClient} from "aws-sdk/clients/dynamodb";
 
-interface User {
-  userId?: string;
-  nickname: string;
-  mail: string;
-}
-interface Bet {
-  betId?: string;
-  title: string;
-  description: string;
-  criteria: Array<{ criteriaTitle: string; criteriaDescription: string }>;
-  expirationTime: string;
-  creator: User;
-  minimumWager: number;
-  competitors: Array<User>;
-}
+const db = new DocumentClient(
+    {
+        endpoint: "http://localstack:4566",
+    }
+);
 
-const handler: ProxyHandler = async (event, context, callback) => {
-  return { statusCode: 200, body: "{}" };
+const handler: APIGatewayProxyHandler = async (event, context, callback) => {
+    // Parse Body - if body doesn't exist (for some reason), return 400
+    if(!event.body) {
+        return {statusCode: 400, body: JSON.stringify({error: "no body"})}
+    }
+    const betData = JSON.parse(event.body) as Bet
+
+    // Generate ID
+    betData.betId = uuidv4()
+
+    // Save to DB
+
+    try{
+        await db.put({Item: {...betData, PK: betData.betId, SK: betData.betId}, TableName: "Bets"}).promise()
+    }catch (e) {
+        return {statusCode: 500, body: JSON.stringify({error: "Database Error", message: JSON.stringify(e)})}
+    }
+
+    return {statusCode: 200, body: JSON.stringify({success: true, data: betData})}
 };
 
 module.exports.handler = handler;

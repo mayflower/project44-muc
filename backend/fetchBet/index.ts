19e774bc19b6fa7733fa84258a8f942cef87834c
Ruben Kaiser <ruben2412000@gmail.com>
2022-09-02 16:21:00 +0200

    Fixed fetchBet route

--- backend/fetchBet/index.ts
+++ backend/fetchBet/index.ts
@@ -1,5 +1,4 @@
 import {APIGatewayProxyHandler} from 'aws-lambda'
-import {Bet} from "./interfaces";
 import {DocumentClient} from "aws-sdk/clients/dynamodb";
 
 const db = new DocumentClient(
@@ -9,30 +8,29 @@ const db = new DocumentClient(
 );
 
 const handler: APIGatewayProxyHandler = async (event, context, callback) => {
-   // Parse Body - if body doesn't exist (for some reason), return 400
-   if(!event.body) {
-    return {statusCode: 400, body: JSON.stringify({error: "no body"})}
-}
-const betId = JSON.parse(event.body) as string
+    // Parse Body - if body doesn't exist (for some reason), return 400
+    if (!event.pathParameters || !event.pathParameters.betId) {
+        return {statusCode: 400, body: JSON.stringify({error: "no body"})}
+    }
+    // const betId = JSON.parse(event.body) as string
+    const betId = event.pathParameters.betId
 
 // Lookup ID in DB
-const params = {
- TableName : "Bets", 
- Key: {
-    HashKey: "betId"
- }
-}
+    const params = {
+        TableName: "Bets",
+        Key: {
+            PK: betId,
+            SK: betId
+        }
+    }
 
 
-
-try{
-    await db.get(params).promise()
-}catch (e) {
-    return {statusCode: 500, body: JSON.stringify({error: "Database Error", message: JSON.stringify(e)})}
-}
-
-return {statusCode: 200, body: JSON.stringify({success: true, data: betData})}
-
+    try {
+        const data = await db.get(params).promise()
+        return {statusCode: 200, body: JSON.stringify({success: true, data: data.Item})}
+    } catch (e) {
+        return {statusCode: 500, body: JSON.stringify({error: "Database Error", message: JSON.stringify(e)})}
+    }
 }
 
 module.exports.handler = handler;
